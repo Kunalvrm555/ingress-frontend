93dd284cd4936bb24f755345b0d2a2575ce01738
Kunal Verma <kunalvrm555@gmail.com>
2023-08-04 19:33:33 +0530

    feat: Add AdminPage, LoginPage, and UserContext components
    This commit adds the following components:
    - AdminPage
    - LoginPage
    - UserContext
    
    The `AdminPage` component is a simple functional component that displays a static message.
    
    The `LoginPage` component is a form that allows users to enter their username and password to log in. It uses the `UserContext` to update the user state after successful login.
    
    The `UserContext` is a React context that provides a way to share user state across different components.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
+import UserContext from './UserContext';
+import LoginPage from './LoginPage';
+import AdminPage from './AdminPage';
 import HomePage from './HomePage';
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState(null);
+
+  // Check for token at start up
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      try {
+        const { username, userType, exp } = jwtDecode(token);
+        if (Date.now() >= exp * 1000) throw new Error('Expired token');
+        setUser({ role: userType, username });
+      } catch (err) {
+        localStorage.removeItem('token');
+        setUser(null);
+      }
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <UserContext.Provider value={{ user, setUser }}>
+        <Routes>
+          <Route path="/" element={user ? user.role === 'admin' ? <AdminPage /> : <HomePage /> : <Navigate to="/login" />} />
+          <Route path="/login" element={<LoginPage />} />
+          {/* <Route path="/admin" element={user?.role === 'admin' ? <AdminPage /> : <Navigate to="/login" />} /> */}
+          {/* <Route path="/user" element={user?.role === 'user' ? <HomePage /> : <Navigate to="/login" />} /> */}
+        </Routes>
+      </UserContext.Provider>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/LoginPage.js
@@ -0,0 +1,94 @@
+import React, { useContext, useState, useEffect } from "react";
+import UserContext from './UserContext';
+import jwtDecode from "jwt-decode";
+import { Button, TextField, Container, Typography, Snackbar } from '@mui/material';
+import { Alert } from '@mui/material';
+import { useNavigate } from "react-router-dom";
+
+const LoginPage = () => {
+  const { setUser } = useContext(UserContext);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const navigate = useNavigate();
+
+  // Check for an existing token in localStorage when the component mounts
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const userRole = jwtDecode(token).userType;
+      setUser({ role: userRole });
+      navigate("/");
+    }
+  }, [navigate, setUser]);
+
+  const handleLogin = async () => {
+    const response = await fetch('http://localhost:8000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        password
+      })
+    });
+
+    if (response.ok) {
+      const { token } = await response.json();
+      localStorage.setItem('token', token);
+      const userRole = jwtDecode(token).userType;
+      setUser({ role: userRole });
+      navigate("/");
+    } else if (response.status === 400) {
+      setOpenSnackbar(true);
+    }
+  };
+
+  const handleCloseSnackbar = () => {
+    setOpenSnackbar(false);
+  };
+
+  return (
+    <Container maxWidth="xs">
+      <Typography variant="h4" align="center">
+        Login
+      </Typography>
+      <TextField 
+        fullWidth 
+        margin="normal" 
+        label="Username" 
+        variant="outlined" 
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <TextField 
+        fullWidth 
+        margin="normal" 
+        label="Password" 
+        type="password" 
+        variant="outlined" 
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button 
+        fullWidth 
+        variant="contained" 
+        color="primary" 
+        onClick={handleLogin}
+      >
+        Login
+      </Button>
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={5000}
+        onClose={handleCloseSnackbar}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
+        <Alert onClose={handleCloseSnackbar} severity="error" sx={{ width: '100%' }}>
+          Invalid username or password.
+        </Alert>
+      </Snackbar>
+    </Container>
+  );
+};
+
+export default LoginPage;
