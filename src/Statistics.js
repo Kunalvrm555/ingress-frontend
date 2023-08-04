8a0bcbf05a85bcdabb8be98cebbc6fa6fcbb3b4c
Kunal Verma <kunalvrm555@gmail.com>
2023-08-04 15:42:43 +0530

    Refactor Statistics and Status components to use Material-UI styling and update their respective UI layouts.
    The Statistics component now fetches data from an API endpoint and displays the statistics in a styled table.
    
    The Status component also displays student information in a styled table and includes a real-time input field for scanning ID cards.

--- src/Statistics.js
+++ src/Statistics.js
@@ -1,11 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Paper,
+  TableContainer,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-const Statistics = ({ checkedInCount }) => {
-    return (
-        <div>
-            <h2>Currently Checked-In Students: {checkedInCount}</h2>
-        </div>
-    );
+const theme = createTheme({
+  typography: {
+    fontFamily: "Arial",
+  },
+});
+const Statistics = ({ lastUpdated }) => {
+  const [statistics, setStatistics] = useState({
+    checkedInCount: 0,
+    checkedInTodayCount: 0,
+  });
+
+  useEffect(() => {
+    fetch("http://localhost:8000/statistics") // replace with your API endpoint
+      .then((res) => res.json())
+      .then((data) => setStatistics(data));
+  }, [lastUpdated]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Box sx={{ maxWidth: "80%",  marginTop: 8, marginBottom: 2, marginLeft: 8 }}
+      >
+        <TableContainer component={Paper} style={{ margin: "10px" }}>
+          <Table>
+            <TableHead style={{ backgroundColor: "#f5f5f5" }}>
+              <TableRow>
+                <TableCell>Statistics</TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell component="th" scope="row">
+                  <Typography variant="subtitle1" color="textSecondary">
+                    Checked-In
+                  </Typography>
+                </TableCell>
+                <TableCell>{statistics.checkedInCount}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell component="th" scope="row">
+                  <Typography variant="subtitle1" color="textSecondary">
+                    Total Today
+                  </Typography>
+                </TableCell>
+                <TableCell>{statistics.checkedInTodayCount}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </ThemeProvider>
+  );
 };
 
 export default Statistics;
--- src/Status.js
+++ src/Status.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect, useCallback } from "react";
 import PropTypes from "prop-types";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import {
   Table,
   TableBody,
   TableCell,
+  TableHead,
   TableContainer,
   TableRow,
   Paper,
@@ -11,6 +13,12 @@ import {
   Typography,
 } from "@mui/material";
 import "./Status.css";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Arial",
+  },
+});
 
 const Status = ({ setLastUpdate }) => {
   const [student, setStudent] = useState({
@@ -22,7 +30,8 @@ const Status = ({ setLastUpdate }) => {
   const [loading, setLoading] = useState(false);
   const [realTimeInput, setRealTimeInput] = useState("");
 
-  const handleKeyPress = useCallback(async (event) => {
+  const handleKeyPress = useCallback(
+    async (event) => {
       if (loading) {
         return;
       }
@@ -72,42 +81,51 @@ const Status = ({ setLastUpdate }) => {
   }, [handleKeyPress]);
 
   return (
-    <Box
-      sx={{ maxWidth: "90%", margin: "auto", marginTop: 2, marginBottom: 2 }}
-    >
-      <TableContainer component={Paper}>
-        <Table
-          sx={{ minWidth: 650 }}
-          aria-label="simple table"
-          className={loading ? "blur-effect" : ""}
-        >
-          <TableBody>
-            <TableRow>
-              <TableCell>
-                <Typography variant="h6">Please scan your ID card</Typography>
-              </TableCell>
-              <TableCell>{realTimeInput}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Roll No</TableCell>
-              <TableCell>{student.rollNo}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell>{student.name}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Check In Time</TableCell>
-              <TableCell>{student.checkInTime}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Check Out Time</TableCell>
-              <TableCell>{student.checkoutTime}</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box
+        sx={{ maxWidth: "90%", margin: "auto", marginTop: 2, marginBottom: 2, marginLeft: 10 }}
+      >
+        <TableContainer component={Paper}>
+          <Table
+            sx={{ minWidth: 650 }}
+            aria-label="simple table"
+            className={loading ? "blur-effect" : ""}
+          >
+            <TableHead style={{ backgroundColor: "#f5f5f5" }}>
+              <TableRow>
+                <TableCell colSpan={2}>
+                  <Typography variant="h6">
+                    Please scan your ID card :
+                    <span style={{ color: "#000000", marginLeft: "10px" }}>
+                      {realTimeInput}
+                    </span>
+                  </Typography>
+                </TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>Roll No</TableCell>
+                <TableCell>{student.rollNo}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>{student.name}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Check In Time</TableCell>
+                <TableCell>{student.checkInTime}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Check Out Time</TableCell>
+                <TableCell>{student.checkoutTime}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </ThemeProvider>
   );
 };
 
